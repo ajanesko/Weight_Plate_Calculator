69ea88c163ac5b4b17c35b3a9326e34850aa41c9
Alex <janeskoa@gmail.com>
2019-09-15 14:36:44 -0500

    first commit

---
+++ app/src/main/java/com/example/weightplatecalculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.weightplatecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ///Test comment for commit
+    }
+}
